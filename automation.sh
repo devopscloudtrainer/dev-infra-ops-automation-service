eb982315fcca65cef9a7db868101da0c7370a533
GoudSagar <goud.sagar.t@gmail.com>
2026-01-27 04:26:40 +0000

    Ticket-76262 Updating automation.sh

--- automation.sh
+++ automation.sh
@@ -1,1 +1,16 @@
-#TO DO
+        f5
+
+[root@ip-172-31-4-95 aws-operation-repo]# git clean -n
+Would remove f1
+Would remove f2
+Would remove f3
+Would remove f4
+Would remove f5
+[root@ip-172-31-4-95 aws-operation-repo]# git clean -f
+Removing f1
+Removing f2
+Removing f3
+Removing f4
+Removing f5
+[root@ip-172-31-4-95 aws-operation-repo]# 
+==============================================
